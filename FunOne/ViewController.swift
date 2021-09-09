165f640d7946959b504c6bc1092b17dc49ddfe9b
amor-tsai <8969139+amor-tsai@users.noreply.github.com>
2021-09-08 21:48:39 -0500

    Add a function to read plilst

--- FunOne/ViewController.swift
+++ FunOne/ViewController.swift
@@ -9,12 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        self.TextView.text = "Hello!"
+        
+        print("this is a dictionary: \(self.getData() as AnyObject)")
+        
     }
 
+    func getData() -> [[String: Any]]? {
+        var data: [[String: Any]]?
+        if let resourcePlistPath = Bundle.main.url(forResource: "Pokemon", withExtension: "plist") {
+            do {
+                let resourcePlistPath = try Data(contentsOf: resourcePlistPath)
+//                if let dict : NSDictionary = NSDictionary(contentsOf: infoPlistPath){
+                if let dict = try PropertyListSerialization.propertyList(from: resourcePlistPath, options: [], format: nil) as? [[String: Any]] {
+                    data = dict
+                }
+            } catch {
+                print(error)
+            }
+        }
+        return data
+        
+    }
+    
 
+    
 }
 
