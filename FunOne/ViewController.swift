8b3e8db14b7bdb51c386aff3d0b63115fd34cf48
amor-tsai <8969139+amor-tsai@users.noreply.github.com>
2021-09-10 00:51:29 -0500

    Add Second Story Board

--- FunOne/ViewController.swift
+++ FunOne/ViewController.swift
@@ -20,24 +20,50 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var WelcomeButton: UIButton!
     
+    @IBOutlet weak var GzcButton: UIButton!
+        
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        self.WelcomeLable.text = "Hello, welcome!"
-        self.WelcomeButton.setTitle("Open them!", for: .normal)
+        self.welcomeLableShow()
+        
+        self.welComeButtonShow()
+        
+        self.gzcButtonShow()
+        
         
         if  let dataDict = self.tableViewModel.getDataDictionary(){
            print(dataDict)
            
-           self.ImageView.sd_setImage(with: URL(string: "https://serebii.net/pokearth/sprites/gold/101.png"), placeholderImage: UIImage(named: "#101.png"))//to be adjusted to the right image
+           self.ImageView.sd_setImage(with: URL(string: "https://serebii.net/pokearth/sprites/gold/101.png"), placeholderImage: UIImage(named: "#101.png"))//to be replaced
 
         }
         
         
     }
-
+    
+    func welcomeLableShow() {
+        //WelcomeLable
+        self.WelcomeLable.text = "Hello, welcome!"
+        self.WelcomeLable.textAlignment = .center
+    }
+    
+    func welComeButtonShow() {
+        //WelcomeButton
+        self.WelcomeButton.setTitle("Open them!", for: .normal)
+        self.WelcomeButton.backgroundColor = .clear
+        self.WelcomeButton.layer.borderWidth = 1
+        self.WelcomeButton.layer.borderColor = UIColor.blue.cgColor
+    }
+    
+    func gzcButtonShow() {
+        self.GzcButton.setTitle("kick off!", for: .normal)
+        self.GzcButton.backgroundColor = .clear
+        self.GzcButton.layer.borderColor = UIColor.blue.cgColor
+        self.GzcButton.layer.borderWidth = 1
+    }
 
     
 
