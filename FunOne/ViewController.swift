bec990792010e0a462459d2d33c5340aea9dd8e0
amor-tsai <8969139+amor-tsai@users.noreply.github.com>
2021-09-09 22:06:22 -0500

    Add the first interface to be start

--- FunOne/ViewController.swift
+++ FunOne/ViewController.swift
@@ -16,16 +16,22 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var ImageView: UIImageView!
     
+    @IBOutlet weak var WelcomeLable: UILabel!
+    
+    @IBOutlet weak var WelcomeButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        self.WelcomeLable.text = "Hello, welcome!"
+        self.WelcomeButton.setTitle("Open them!", for: .normal)
         
         if  let dataDict = self.tableViewModel.getDataDictionary(){
            print(dataDict)
            
-           self.ImageView.sd_setImage(with: URL(string: "https://serebii.net/pokearth/sprites/gold/101.png"), placeholderImage: UIImage(named: "#101.png"))
+           self.ImageView.sd_setImage(with: URL(string: "https://serebii.net/pokearth/sprites/gold/101.png"), placeholderImage: UIImage(named: "#101.png"))//to be adjusted to the right image
 
         }
         
