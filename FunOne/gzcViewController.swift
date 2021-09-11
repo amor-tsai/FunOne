73a484e219468da6c25c6b7d9c230071af5e1625
gzccc <zichengg@uci.edu>
2021-09-10 21:03:38 -0500

    Second Storyboard Version 1.1

--- FunOne/gzcViewController.swift
+++ FunOne/gzcViewController.swift
@@ -6,8 +6,23 @@
 //
 
 import UIKit
-
+let tm = TableModel()
+let dict = tm.getDataDictionary()
 class gzcViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UIScrollViewDelegate {
+    
+    @IBOutlet weak var choosePokemonPicker: UIPickerView?
+    @IBOutlet weak var valueLbl: UILabel?
+    @IBOutlet weak var valueLbl1: UILabel?
+    @IBOutlet weak var valueLbl2: UILabel?
+    @IBOutlet weak var valueLbl3: UILabel?
+    @IBOutlet weak var stepper: UIStepper?
+    @IBOutlet weak var stepper1: UIStepper?
+    @IBOutlet weak var stepper2: UIStepper?
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var ATK: UILabel!
+    @IBOutlet weak var DEF: UILabel!
+    @IBOutlet weak var SPD: UILabel!
+    @IBOutlet weak var HP: UILabel!
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -18,12 +33,21 @@ class gzcViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDat
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return pokemons[row]
+        return (pokemons[row] as! String)
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        pokemonName?.text = pokemons[row]
-        pokemonImage?.image = UIImage(named: pokemons[row])
+        pokemonName?.text = (pokemons[row] as! String)
+        pokemonImage?.image = tm.getImageWithName(name: pokemons[row] as! String)
+        print(dict![row]["ATT"]!)
+        valueLbl?.text = "\(dict![row]["ATT"]!)" as String
+        stepper?.value = dict![row]["ATT"]! as! Double
+        valueLbl1?.text = "\(dict![row]["DEF"]!)" as String
+        stepper1?.value = dict![row]["DEF"]! as! Double
+        valueLbl2?.text = "\(dict![row]["Speed"]!)" as String
+        stepper2?.value = dict![row]["Speed"]! as! Double
+        valueLbl3?.text = "\(dict![row]["HP"]!)" as String
+        slider?.value = dict![row]["HP"]! as! Float
     }
     
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
@@ -33,17 +57,10 @@ class gzcViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDat
     @IBOutlet weak var pokemonImage: UIImageView?
     @IBOutlet weak var pokemonName: UILabel?
     
-    let pokemons = ["bulbasaur","pikachu"]
+    var pokemons = [] as NSMutableArray
     
-    @IBOutlet weak var choosePokemonPicker: UIPickerView?
+
     
-    
-    @IBOutlet weak var valueLbl: UILabel?
-    @IBOutlet weak var valueLbl1: UILabel?
-    @IBOutlet weak var valueLbl2: UILabel?
-    @IBOutlet weak var valueLbl3: UILabel?
-    @IBOutlet weak var stepper: UIStepper?
-    @IBOutlet weak var stepper1: UIStepper?
     @IBAction func slider(_ sender: UISlider) {
         valueLbl3?.text = String(Int(sender.value))
     }
@@ -63,6 +80,12 @@ class gzcViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDat
 
     
     override func viewDidLoad() {
+        for element in dict!{
+            pokemons.add(element["Name"]!)
+        }
+        print(dict![1]["ATT"]!)
+        print(type(of: dict![1]["ATT"]!))
+        
         super.viewDidLoad()
         choosePokemonPicker?.delegate = self
         choosePokemonPicker?.dataSource = self
@@ -71,10 +94,10 @@ class gzcViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDat
         
         stepper?.autorepeat = true
         stepper?.isContinuous = true
-        valueLbl?.text = "0"
-        valueLbl1?.text = "0"
-        valueLbl2?.text = "0"
-        valueLbl3?.text = "0"
+        ATK.text = "ATT:"
+        DEF.text = "DEF:"
+        SPD.text = "SPD:"
+        HP.text = "HP:"
         stepper?.maximumValue = 100
         stepper?.minimumValue = 0
         
